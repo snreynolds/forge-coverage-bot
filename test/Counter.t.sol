2b5c7d694de4e112c8f462c746dff5cddf8b003d
Sara Reynolds <snreynolds2506@gmail.com>
2024-02-07 13:31:19 -0500

    test again

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -7,7 +7,7 @@ import {Counter} from "../src/Counter.sol";
 contract CounterTest is Test {
     Counter public counter;
 
-    function setUp2() public {
+    function setUp3() public {
         counter = new Counter();
         counter.setNumber(0);
     }
